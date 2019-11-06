6cca6861e91f03e3db19c826798713458ce28795
Eldar Shmatkov <eldarshmatkov@gmail.com>
2019-11-06 23:28:10 +0300

    quest complete

--- src/components/TableContent.vue
+++ src/components/TableContent.vue
@@ -1,114 +1,113 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+    <div class="table-content-wrapper self-center -mt-40 sm:-mt-20 text-sm sm:text-base  rounded-lg shadow-lg overflow-hidden w-11/12">
+        <div class="flex flex-row bg-gray-200 p-2 border-b border-gray-400 cursor-pointer">
+            <div class="flex flex-1 justify-center items-center table-title">Rank</div>
+            <div class="flex flex-1 sm:flex-3 justify-center items-center table-title">Name</div>
+            <div class="flex flex-1 justify-center items-center table-title">Price</div>
+            <div class="hidden lg:flex flex-1 justify-center items-center table-title">Market Cap</div>
+            <div class="hidden lg:flex flex-1 justify-center items-center table-title">VWAP (24Hr)</div>
+            <div class="hidden lg:flex flex-1 justify-center items-center table-title">Supply</div>
+            <div class="hidden lg:flex flex-1 justify-center items-center table-title">Volume (24Hr)</div>
+            <div class="hidden lg:flex flex-1 justify-center items-center table-title">Change (24Hr)</div>
+        </div>
+        <div class="table-content" v-if="currency.data && currency">
+            <div class="flex flex-row bg-white p-2 border-b border-gray-400 cursor-pointer"
+                 v-for="(item, index) of currency.data.slice(0, 15)" :key="index">
+                <div class="flex flex-1 justify-center">{{item.rank}}</div>
+                <div class="flex flex-1 sm:flex-3 justify-center">{{item.name}}</div>
+                <div class="flex flex-1 justify-center">{{item.priceUsd | currency}}</div>
+                <div class="hidden lg:flex flex-1 justify-center">{{item.marketCapUsd | convertNumbers}}</div>
+                <div class="hidden lg:flex flex-1 justify-center">{{item.vwap24Hr | currency}}</div>
+                <div class="hidden lg:flex flex-1 justify-center">{{item.supply | convertNumbers}}</div>
+                <div class="hidden lg:flex flex-1 justify-center">{{item.volumeUsd24Hr | convertNumbers}}</div>
+                <div class="hidden lg:flex flex-1 justify-center" :class="checkIncome(item.changePercent24Hr)">{{
+                    Number(item.changePercent24Hr).toFixed(2) + '%'}}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
+    import axios from 'axios';
+
+    export default {
+        data: () => ({
+            currency: [],
+            errors: [],
+            ws: null
+        }),
+        methods: {
+            checkIncome: function (number) {
+                let checkNumberAffinity = Math.sign(Number(number));
+                if (checkNumberAffinity > 0) {
+                    return 'text-green-600'
+                } else if (checkNumberAffinity < 0) {
+                    return 'text-red-600'
+                }
+            }
+        },
+        created() {
+            axios.get('https://api.coincap.io/v2/assets')
+                .then(response => {
+                    this.currency = response.data
+                })
+                .catch(e => {
+                    this.errors.push(e);
+                });
+            let self = this;
+
+            this.ws = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin');
+
+            this.ws.onmessage = function (msg) {
+                let parsedJson = JSON.parse(msg.data);
+                Object.keys(parsedJson)
+                    .forEach(function eachKey(key) {
+                        let getSimilarItemIndex = self.currency.data.findIndex(x => x.id === key);
+                        self.currency.data[getSimilarItemIndex].priceUsd = parsedJson[key]
+                    });
+            };
+        },
+        filters: {
+            convertNumbers: function (labelValue) {
+                // Nine Zeroes for Billions
+                return Math.abs(Number(labelValue)) >= 1.0e+9
+
+                    ? '$ ' + (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
+                    // Six Zeroes for Millions
+                    : Math.abs(Number(labelValue)) >= 1.0e+6
+
+                        ? '$ ' + (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
+                        // Three Zeroes for Thousands
+                        : Math.abs(Number(labelValue)) >= 1.0e+3
+
+                            ? '$ ' + (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
+
+                            : Math.abs(Number(labelValue));
+
+            }
+        }
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .table-content {
+        height: calc(100vh - 5rem - 163px);
+        max-height: 639px;
+        overflow: scroll;
+
+        .table-title {
+            &::after {
+                content: '';
+            }
+        }
+    }
+
+    @media (max-width: 1024px) {
+        .table-content {
+            height: calc(100vh - 61px);
+            max-height: 570px;
+        }
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home flex flex-col pb-10 w-screen">
+        <header class="pt-10 pb-32 pl-2 pr-2">
+            <div class="flex-row wrapper hidden sm:flex">
+                <div class="w-1/6 flex flex-col text-white">
+                    MARKET CAP
+                    <span>$</span>
+                </div>
+                <div class="w-1/6 flex flex-col text-white">
+                    EXCHANGE VOL
+                    <span>$</span>
+                </div>
+                <div class="w-1/6 flex flex-col text-white">
+                    ASSETS
+                    <span>$</span>
+                </div>
+                <div class="w-1/6 flex flex-col text-white">
+                    EXCHANGES
+                    <span>$</span>
+                </div>
+                <div class="w-1/6 flex flex-col text-white">
+                    MARKETS
+                    <span>$</span>
+                </div>
+                <div class="w-1/6 flex flex-col text-white">
+                    BTC DOM INDEX
+                    <span>$</span>
+                </div>
+            </div>
+        </header>
+        <TableContent msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+    // @ is an alias to /src
+    import TableContent from "@/components/TableContent.vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: "home",
+        components: {
+            TableContent
+        }
+    };
 </script>
+
+<style scoped lang="scss">
+    header {
+        background: linear-gradient(to right, rgb(63, 81, 181), rgb(100, 181, 246)) rgb(255, 255, 255);
+    }
+</style>
